a67530f7d9b1b160ff0811d041a7860a397c4f7a
mohamed <mehdaoui200309@gmail.com>
2024-06-26 16:38:39 +0100

    "Formatted SiteRequest.php code, added whitespace and fixed syntax errors."

--- app/Http/Requests/SiteRequest.php
+++ app/Http/Requests/SiteRequest.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Http\Requests;
+
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
 class SiteRequest extends FormRequest
 {
-   /**
+    /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
@@ -23,28 +24,29 @@ class SiteRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'description'=>'required |string',
-            'titre'=>'required|string',
-            'lien'=>'required| string',
-            'id_cat'=>'required'
+            'description' => 'required|string',
+            'titre' => 'required|string',
+            'lien' => 'required|string',
+            'id_cat' => 'required'
 
         ];
     }
-    public function failedValidation(Validator $validator){
+    public function failedValidation(Validator $validator)
+    {
         return redirect('create')->withErrors($validator)->withInput();
     }
     public function messages()
     {
-        return[
-            'description.required '=>'description non fourni',
-            'description.string'=>"le description  doit être un string",
-            'titre.required '=>'Titre non fourni',
-            'titre.string'=>"le Titre  doit être un string",
-            'logo.required '=>'logo non fourni',
-            'logo.string'=>"le logo  doit être un string",
-            'lien.required '=>'lien non fourni',
-            'lien.string'=>"le lien  doit être un string",
-            
+        return [
+            'description.required ' => 'description non fourni',
+            'description.string' => "le description  doit être un string",
+            'titre.required ' => 'Titre non fourni',
+            'titre.string' => "le Titre  doit être un string",
+            'logo.required ' => 'logo non fourni',
+            'logo.string' => "le logo  doit être un string",
+            'lien.required ' => 'lien non fourni',
+            'lien.string' => "le lien  doit être un string",
+
         ];
     }
 }
